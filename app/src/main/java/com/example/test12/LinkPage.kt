e32bdaabda721f6ca0f755505974c388fb889505
jsm <wlstmdals12@naver.com>
2022-08-05 13:52:11 +0900

    Add search_icon to LinkPage_toolbar

--- app/src/main/java/com/example/test12/LinkPage.kt
+++ app/src/main/java/com/example/test12/LinkPage.kt
@@ -2,10 +2,14 @@ package com.example.test12
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_linkpage.*
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_setting.*
 import kotlinx.android.synthetic.main.callhistory.*
 class LinkPage : AppCompatActivity(){
     val DataList = arrayListOf(
@@ -20,13 +24,26 @@ class LinkPage : AppCompatActivity(){
 
         setSupportActionBar(LinkPage_toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        LinkPage_toolbar.title = "보호자 찾기"
 
         user_recyclerView.layoutManager = LinearLayoutManager(this)
         user_recyclerView.adapter = CustomAdapter(DataList)
     }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val myintent1 = Intent(this,MainActivity::class.java)
-        startActivity(myintent1)
+        when(item?.itemId){
+            android.R.id.home ->{
+                finish()
+                return true
+            }
+            R.id.action_search ->{
+                Toast.makeText(applicationContext,"검색 이벤트 실행",Toast.LENGTH_LONG).show()
+                return super.onOptionsItemSelected(item)
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.search_toolbar_menu,menu)
         return true
     }
 }
